78ef062b150eff78c2e10b6562f553d9cc7857b4
DariaHellscat <72994558+DariaHellscat@users.noreply.github.com>
2021-09-13 22:35:13 +0300

    Digital Designer Landing Page

--- Digital Designer/js/main.js
+++ Digital Designer/js/main.js
@@ -1,5 +1,6 @@
 $(function () {
   $(".menu__btn").on("click", function () {
     $(".menu__list").toggleClass("menu__list--active");
+    $(".header__top-inner").toggleClass("header__top-inner--active");
   });
 });
